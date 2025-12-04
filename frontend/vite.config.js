da5d31f308c4d40f6070bf75de1b92ca72194915
Suhani_Gupta <suhu786.gupta@gmail.com>
2025-12-04 23:14:18 +0530

    Configure Vite preview for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5173}
+  
+  // For local development
+  server: {
+    port: 5173
+  },
+
+  // For Render deployment
+  preview: {
+    port: process.env.PORT || 4173, // use Render's port
+    host: true, // allow external access
+    allowedHosts: ['prescripto-frontend-llch.onrender.com'] // your Render URL
+  }
 })
